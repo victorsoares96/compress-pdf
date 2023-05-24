6a478155695ab6992ed09708685793891e216790
João Victor Moreira Soares <vitorsoares96@hotmail.com>
2023-05-24 15:19:03 -0300

    Update basic.ts

--- examples/basic.ts
+++ examples/basic.ts
@@ -2,13 +2,6 @@ import path from 'path';
 import fs from 'fs';
 import { compress } from '@/index';
 
-/**
- * Before run:
- *
- * `npx compress-pdf --fetchBinaries [win32/linux]`
- * This will download properly binaries to your current os.
- */
-
 (async () => {
   const pdf = path.resolve(__dirname, 'A17_FlightPlan.pdf');
   const buffer = await compress(pdf);
